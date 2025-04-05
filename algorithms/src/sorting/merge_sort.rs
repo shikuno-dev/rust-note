b7f608d9ef8960528d833eca59b94530ba767afa
shikuno-dev <shir01earn@gmail.com>
2025-04-05 21:47:06 +0900

    Implemented merge sort under sorting

---
+++ algorithms/src/sorting/merge_sort.rs
@@ -0,0 +1,38 @@
+fn merge_sort(arr: &mut [i32]) {
+    let len = arr.len();
+    if len < 2 {
+        return;
+    }
+    let mid = len / 2;
+    let mut left = arr[..mid].to_vec();
+    let mut right = arr[mid..].to_vec();
+    merge_sort(&mut left);
+    merge_sort(&mut right);
+    let (mut i, mut j, mut k) = (0, 0, 0);
+    while i < left.len() && j < right.len() {
+        if left[i] < right[j] {
+            arr[k] = left[i];
+            i += 1;
+        } else {
+            arr[k] = right[j];
+            j += 1;
+        }
+        k += 1;
+    }
+    while i < left.len() {
+        arr[k] = left[i];
+        i += 1;
+        k += 1;
+    }
+    while j < right.len() {
+        arr[k] = right[j];
+        j += 1;
+        k += 1;
+    }
+}
+
+fn main() {
+    let mut arr = [5, 3, 8, 4, 2, 1];
+    merge_sort(&mut arr);
+    println!("{:?}", arr);
+}
